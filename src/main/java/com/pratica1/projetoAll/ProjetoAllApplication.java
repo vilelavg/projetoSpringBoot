b582b1a8c361c094a5681806ee692cdc40596d96
Vitor Vilela <vitorvilella123@gmail.com>
2023-10-25 15:23:33 -0300

    Projet created

---
+++ src/main/java/com/pratica1/projetoAll/ProjetoAllApplication.java
@@ -0,0 +1,13 @@
+package com.pratica1.projetoAll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratica1/projetoAll/ProjetoAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratica1.projetoAll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
