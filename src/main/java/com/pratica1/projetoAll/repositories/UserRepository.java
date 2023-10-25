1f20b104f75a3dd27e9b74b2bed82e2778aa85f6
Vitor Vilela <vitorvilella123@gmail.com>
2023-10-25 16:26:09 -0300

    UserRepository

---
+++ src/main/java/com/pratica1/projetoAll/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pratica1.projetoAll.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pratica1.projetoAll.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+}
